729b112bc09b6219b242aef21fffd8a694e1b82a
Thomas Kilgour <t.kilgour@gmail.com>
2017-01-16 17:17:54 -0500

    added downloadImageByURL function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var GITHUB_USER = 'tkilgour';
 var GITHUB_TOKEN = 'f9e6087b99016c107ce10b650785d6c884038460';
 
@@ -8,13 +9,28 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {'user-agent': 'GitHub Avatar Downloader - Student Project'}
-  }
+  };
 
   request(options, function (error, response, body) {
     cb(error, JSON.parse(body));
   })
+};
 
+function downloadImageByURL (url, filePath) {
+  request.get(url)
+         .on('error', function (err) {
+           throw err;
+         })
+         .on('response', function (response) {
+           console.log('Downloading image...');
+         })
+         .on('end', function () {
+           console.log('Download complete.');
+         })
+         .pipe(fs.createWriteStream(filePath));
 }
+
+downloadImageByURL ('https://avatars.githubusercontent.com/u/1615?v=3', './avatars/tmp.jpg');
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   result.forEach(function (user) {
