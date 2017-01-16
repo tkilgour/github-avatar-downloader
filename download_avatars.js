9d5317dcf3297a218c0c5d6d08aa03326e4adfbc
Thomas Kilgour <t.kilgour@gmail.com>
2017-01-16 16:25:59 -0500

    added request that logs the body of response. also added user-agent as option in header

--- download_avatars.js
+++ download_avatars.js
@@ -5,11 +5,18 @@ var GITHUB_TOKEN = 'f9e6087b99016c107ce10b650785d6c884038460';
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var options = {
+    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {'user-agent': 'GitHub Avatar Downloader - Student Project'}
+  }
+
+  request(options, function (error, response, body) {
+    console.log(JSON.parse(body));
+  })
+
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, result, body) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
