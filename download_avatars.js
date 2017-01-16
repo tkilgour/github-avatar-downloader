154f7544289bd1f75a98a0e53b4b73546b505013
Thomas Kilgour <t.kilgour@gmail.com>
2017-01-16 14:59:09 -0500

    added requestURL var to getRepoContributors and logged it

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,12 @@
 var request = require('request');
+var GITHUB_USER = 'tkilgour';
+var GITHUB_TOKEN = 'f9e6087b99016c107ce10b650785d6c884038460';
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
